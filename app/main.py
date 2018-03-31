58fbad75db93a75591c7b3e40c2df7214cc3ca2a
Vladimir Pivnichenko <pivnichenko.amgrade@gmail.com>
2018-03-31 18:57:13 +0300

    add static file

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect, url_for
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World from Flask in a uWSGI Nginx Docker container with \
-     Python 3.6 (from the example template) 123"
+    return render_template('base.html', title='Home')
 
 
 if __name__ == "__main__":
